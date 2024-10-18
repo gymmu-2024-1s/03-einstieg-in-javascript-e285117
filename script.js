5aea3bb02369a60d4efd79dcd43f5b99b208e31d
Javiera Vasquez <e285117@sbl.ch>
2024-10-18 12:31:50 +0000

    Aufgabe 2 teil 1

--- script.js
+++ script.js
@@ -1,32 +1,10 @@
 import { linkupExerciseHandler } from "./utils"
 
-/**
- * Wir erstellen hier eine Funktion für die Aufgabe 1. Funktionen sind praktisch
- * um den Code innerhalb, vom rest zu trennen. Dadurch können wir Variablen mit
- * gleichem Namen auch in anderen Funktionen verwenden, ohne das deren Wert
- * überschrieben wird.
- * Funktionen können auch verwendet werden, um an anderer Stelle wieder
- * verwendet zu werden. Wir machen das in diesem Projekt eigentlich nicht, aber
- * wenn Sie des Resultat von einer Funktion nochmals brauchen, können Sie das
- * wie folgt machen:
- *    `const withoutE = aufgabe01("Hier ist ein Text mit einigen e's")`
- * Damit wird der Code in aufgabe01 ausgeführt, der sollte alle e's entfernen,
- * und das Resultate wird in der Variable `withoutE` gespeichert, und kann dann
- * weiter verwendet werden.
- */
 export function aufgabe01(args) {
-  // Wir speichern hier den Wert von args in der Variable `input` ab. Damit soll für uns klarer werden, womit wir arbeiten.
   const input = args
-
-  // Wir erzeugen hier eine leere Liste, in der wir das Resultat Stück für Stück anhängen.
   const result = []
 
-  // Mit dieser Schlaufe nummerieren wir jedes Zeichen in `input` durch. Das
-  // machen wir um jedes Zeichen einzeln anzuschauen.
   for (let i = 0; i < input.length; i++) {
-    // Hier speichern wir das Zeichen an der Stelle `i` in der Variable
-    // `currentElement`, damit es explizit ist womit wir arbeiten, aber auch
-    // damit wir nicht so viele Klammern schreiben müssen.
     const currentElement = input[i]
 
     if (currentElement === "e") {
@@ -38,7 +16,6 @@ export function aufgabe01(args) {
     }
   }
 
-  // Hier wird das aktuelle Zeichen ans Ende der Resultat-Liste angehängt.
   return result.join("")
 }
 
@@ -46,10 +23,20 @@ linkupExerciseHandler("[data-click=aufgabe01]", aufgabe01)
 
 export function aufgabe02(args) {
   const input = args
-  const result = []
+  const result = [] //Das ist die Resultatliste
 
+  // Lauft Zeich für Zeichen oder den ganzen Text.
   for (let i = 0; i < input.length; i++) {
-    const currentElement = [i]
+    const currentElement = input[i]
+    // Hänge das aktuelle Zeichen doppelt an
+    if (currentElement === "abc") {
+      // do nothing
+    } else if (currentElement === "ABC") {
+      // auch E ignorieren
+    } else {
+      result.push(currentElement)
+    }
+
     result.push(currentElement)
     result.push(currentElement)
   }
